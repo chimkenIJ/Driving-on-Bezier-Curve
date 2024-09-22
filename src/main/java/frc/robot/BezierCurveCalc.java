204a59a70b09964a302af45a7784a6061580822c
Isha <ishaj2018@gmail.com>
2024-09-22 12:50:35 -0700

    more cleanups

--- src/main/java/frc/robot/BezierCurveCalc.java
+++ src/main/java/frc/robot/BezierCurveCalc.java
@@ -1,66 +1,53 @@
 package frc.robot;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class BezierCurveCalc {
- private int orderNum;
- private double step = 0.05;
- List<double[]> points;
- List<double[]> curve;
+  private int orderNum;
+  private double step = 0.05;
+  List<double[]> points;
+  List<double[]> curve;
 
+  public BezierCurveCalc(List<double[]> points) {
+    orderNum = points.size();
+    this.points = points;
+  }
 
- public BezierCurveCalc(List<double[]> points) {
-   orderNum = points.size();
-   this.points = points;
- }
+  public List<double[]> calcCurve() {
+    List<double[]> newPoints = new ArrayList<>();
+    double time = 0;
+    do {
+      newPoints.add(getPoint(time));
+      time += step;
 
+    } while (time < 1);
 
- public List<double[]> calcCurve() {
-   List<double[]> newPoints = new ArrayList<>();
-   double time = 0;
-   do {
-     newPoints.add(getPoint(time));
-     time += step;
+    return newPoints;
+  }
 
+  public double[] getPoint(double time) {
+    double x = 0;
+    double y = 0;
+    for (int i = 0; i < orderNum; i++) {
+      x += 24 * b(time, i, points.get(i)[0]);
+    }
+    for (int i = 0; i < orderNum; i++) {
+      y += 24 * b(time, i, points.get(i)[1]);
+    }
 
-   } while (time < 1);
+    double[] newPoint = {x, y};
 
+    return newPoint;
+  }
 
-   return newPoints;
- }
+  public double b(double time, int K, double v) {
+    return nChooseK(orderNum, K) * Math.pow(1 - time, orderNum - K) * Math.pow(time, K) * v;
+  }
 
-
- public double[] getPoint(double time) {
-   double x = 0;
-   double y = 0;
-   for (int i = 0; i < orderNum; i++) {
-     x += 24 * b(time, i, points.get(i)[0]);
-   }
-   for (int i = 0; i < orderNum; i++) {
-     y += 24 * b(time, i, points.get(i)[1]);
-   }
-
-
-   double[] newPoint = {x, y};
-
-
-   return newPoint;
- }
-
-
- public double b(double time, int K, double v) {
-   return nChooseK(orderNum, K) * Math.pow(1 - time, orderNum - K) * Math.pow(time, K) * v;
- }
-
-
- public double nChooseK(int n, int k) {
-   if (k > n) return 0;
-   if (k == 0 || k == n) return 1;
-   return nChooseK(n - 1, k - 1) + nChooseK(n - 1, k);
- }
+  public double nChooseK(int n, int k) {
+    if (k > n) return 0;
+    if (k == 0 || k == n) return 1;
+    return nChooseK(n - 1, k - 1) + nChooseK(n - 1, k);
+  }
 }
-
-
--- src/main/java/frc/robot/commands/DriveCurve.java
+++ src/main/java/frc/robot/commands/DriveCurve.java
@@ -1,37 +1,31 @@
 package frc.robot.commands;
 
-
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import frc.robot.subsystems.XRPDrivetrain;
 import java.util.List;
 
-
 public class DriveCurve extends SequentialCommandGroup {
- public DriveCurve(List<double[]> curve, XRPDrivetrain drivetrain) {
-   double currX = 0;
-   double currY = 0;
-   double currAng = 0;
-   for (int i = 0; i < curve.size() - 1; i++) {
-     double ang = Math.toDegrees(Math.atan((curve.get(i + 1)[1]) / (curve.get(i + 1)[0])));
-     if (Double.isNaN(ang)) {
-       ang = 0;
-     }
-     currAng = ang - currAng + 90;
-     double dist =
-         (curve.get(i)[0] - currX) * (curve.get(i)[0] - currX)
-             + (curve.get(i)[1] - currY) * (curve.get(i)[1] - currY);
-     System.out.println(curve.get(i)[0] + ", " + curve.get(i)[1]);
+  public DriveCurve(List<double[]> curve, XRPDrivetrain drivetrain) {
+    double currX = 0;
+    double currY = 0;
+    double currAng = 0;
+    for (int i = 0; i < curve.size() - 1; i++) {
+      double ang = Math.toDegrees(Math.atan((curve.get(i + 1)[1]) / (curve.get(i + 1)[0])));
+      if (Double.isNaN(ang)) {
+        ang = 0;
+      }
+      currAng = ang - currAng + 90;
+      double dist =
+          (curve.get(i)[0] - currX) * (curve.get(i)[0] - currX)
+              + (curve.get(i)[1] - currY) * (curve.get(i)[1] - currY);
+      System.out.println(curve.get(i)[0] + ", " + curve.get(i)[1]);
 
+      addCommands(
+          new TurnDegrees(1, currAng, drivetrain), (new DriveDistance(1, dist, drivetrain)));
 
-     addCommands(
-         new TurnDegrees(1, currAng, drivetrain), (new DriveDistance(1, dist, drivetrain)));
-
-
-     currX = curve.get(i)[0];
-     currY = curve.get(i)[1];
-   }
-   System.out.println("done ^-^");
- }
+      currX = curve.get(i)[0];
+      currY = curve.get(i)[1];
+    }
+    System.out.println("done ^-^");
+  }
 }
-
-
